a4e387d67f830e89713c3a1842713b50e6716cfd
Prathamesh Dhawale <dhawale@usc.edu>
2025-08-28 20:21:56 -0700

    Fix package.json merge conflicts and restore frontend dependencies, update App.tsx and Playground.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
-import Home from './pages/Home';
-=======
->>>>>>> 837c1f018fe6b583fd83f89ece7bda14111c4ed8
+// import Home from './pages/Home';
 import Playground from './pages/Playground';
 
 function App() {
   return <Playground />;
-<<<<<<< HEAD
   // return <Home />;
-=======
->>>>>>> 837c1f018fe6b583fd83f89ece7bda14111c4ed8
 }
 
 export default App;
